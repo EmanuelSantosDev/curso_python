2264b1999d6dfa1e5e484330be5ddc2135cdeda1
Emanuel Santos <eu.emanuel.santos@outlook.com>
2023-01-15 17:39:04 -0300

    Update

---
+++ 03 - Funções/02 - Processar e Retornar.py
@@ -0,0 +1,8 @@
+# retornando o TOTAL da operação
+def soma(a, b):
+    total = a + b
+    return total
+
+
+resultado = soma(3, 5)
+print(resultado)
---
+++ 03 - Funções/03 - Argumentos Posicionais VS Argumentos Nomeados.py
@@ -0,0 +1,41 @@
+# Argumentos Posicionais
+
+
+def exibir_preco(nome_produto, preco):
+    print(f'{nome_produto} está no valor de R$ {preco}')
+
+
+exibir_preco('iPhone', 5000)
+exibir_preco(5000, 'iPhone')
+'''
+iPhone está no valor de R$ 5000
+5000 está no valor de R$ iPhone
+'''
+
+'''---------------------------------------------------------------------------'''
+
+# Argumentos Nomeados
+
+
+exibir_preco(preco=5000, nome_produto='iPhone')
+'''iPhone está no valor de R$ 5000'''
+
+'''---------------------------------------------------------------------------'''
+
+# inserindo '*' obriga que todos os próximos argumentos sejam NOMEADOS
+
+
+# TypeError: quantidade() takes 0 positional arguments but 2 were given
+def quantidade(*, a, b):
+    print(a + b)
+
+
+quantidade(2, 5)
+
+
+# / TypeError: multiplicacao() takes 1 positional argument but 2 were given
+def multiplicacao(x, *, y):
+    print(x * y)
+
+
+multiplicacao(4, 3)
