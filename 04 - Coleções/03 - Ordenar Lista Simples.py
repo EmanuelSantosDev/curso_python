07dbfbc488ab57054109104bc27b0bfc062f05c0
Emanuel Santos <eu.emanuel.santos@outlook.com>
2023-01-18 23:38:56 -0300

    Update

---
+++ 04 - Coleções/03 - Ordenar Lista Simples.py
@@ -0,0 +1,19 @@
+# Ordenar Lista Simples
+
+
+# Ordem Crescente
+valores = [31, 23, 6, 36, 21, 33, 37, 7, 20, 23]
+valores.sort()
+print(valores)  # [6, 7, 20, 21, 23, 23, 31, 33, 36, 37]
+
+
+# Ordem Decrescente
+valores2 = [31, 23, 6, 36, 21, 33, 37, 7, 20, 23]
+valores2.sort(reverse=True)
+print(valores2)  # [37, 36, 33, 31, 23, 23, 21, 20, 7, 6]
+
+
+# Invertendo os Valores
+valores3 = [31, 23, 6, 36, 21, 33, 37, 7, 20, 23]
+valores3.reverse()
+print(valores3)  # [23, 20, 7, 37, 33, 21, 36, 6, 23, 31]
---
+++ 04 - Coleções/04 - Processando Múltiplas Listas.py
@@ -0,0 +1,51 @@
+# Processando Múltiplas Listas - zip()
+
+
+# Iterando sobre duas listas simultaneamente com zip()
+from itertools import zip_longest
+a_lista = ['A', 'B', 'C', 'D', 'E']
+b_lista = [1, 2, 3, 4, 5, 6]
+for a, b in zip(a_lista, b_lista):
+    print(a, end=' ')
+    print(b, end=' ')
+    # A 1 B 2 C 3 D 4 E 5
+
+
+# Exemplo nº 2
+produtos = ['Produto 1', 'Produto 2', 'Produto 3', 'Produto 4', 'Produto 5']
+precos = [250, 150, 220, 550, 50]
+for a, b in zip(produtos, precos):
+    print(f'Salvando produto {a} valor R$ {b}')
+'''
+Salvando produto Produto 1 valor R$ 250
+Salvando produto Produto 2 valor R$ 150
+Salvando produto Produto 3 valor R$ 220
+Salvando produto Produto 4 valor R$ 550
+Salvando produto Produto 5 valor R$ 50
+'''
+
+
+# Lista com Quantidade de Itens Diferentes
+titulos = ['Titulo 1', 'Titulo 2', 'Titulo 3', 'Titulo 4']
+descricoes = ['Descrição 1', 'Descrição 2', 'Descrição 3']
+for titulo, descricao in zip_longest(titulos, descricoes):
+    print(f'Encontramos o {titulo} com a descrição: {descricao} ')
+'''
+Encontramos o Titulo 1 com a descrição: Descrição 1
+Encontramos o Titulo 2 com a descrição: Descrição 2
+Encontramos o Titulo 3 com a descrição: Descrição 3
+Encontramos o Titulo 4 com a descrição: None
+'''
+
+# Lista com Quantidade de Itens Diferentes (sem utilizar o zip_longest)
+produtos4 = ['Produto 1', 'Produto 2', 'Produto 3', 'Produto 4', 'Produto 5']
+precos4 = [250, 150, 220]
+for a, b in zip(produtos4, precos4):
+    print(f'Salvando produto {a} valor R$ {b}')
+'''
+Irá 'ignorar' os dois últimos items da lista 'produtos4':
+
+Salvando produto Produto 1 valor R$ 250
+Salvando produto Produto 2 valor R$ 150
+Salvando produto Produto 3 valor R$ 220
+'''
