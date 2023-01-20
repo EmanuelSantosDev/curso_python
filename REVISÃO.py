02322dbc03f01a8a310a64133c759c9f0ec29e03
Emanuel Santos <eu.emanuel.santos@outlook.com>
2023-01-20 07:39:13 -0300

    Update

--- REVISÃO.py
+++ REVISÃO.py
@@ -9,7 +9,7 @@
 # Caracteres de Escape (Não interpretando aspas internas - 2 formas)
 # Caracteres de Escape (Desativando a interpretação da "\")
 # Descobrir o comprimento de uma String
-# Strings Dinâmicos
+# Strings Dinâmicos (f'Texto {variável}')
 # Funções de Strings (letra MAIÚSCULA)
 # Funções de Strings (letra MINÚSCULA)
 # Funções de Strings (retirar ESPAÇOS EM BRANCO)
@@ -17,27 +17,27 @@
 # Funções de Strings (retirar ESPAÇOS EM BRANCOS do lado DIREITO)
 # Funções de Strings (ENCONTRAR O ÍNDICE de determinados caracteres)
 # Funções de Strings (TROCAR uma cadeia de strings por outra)
-# Slice utilizando índices (um ÚNICO índice especifico)
-# Slice utilizando índices (o ÚLTIMO índice)
-# Slice utilizando índices (de um índice INICIAL até um índice FINAL)
-# Slice utilizando índices (de um índice INICIAL até o ÚLTIMO ÍNDICE)
-# Slice utilizando índices (de X ÍNDICES NEGATIVOS até o ÚLTIMO ÍNDICE)
-# Slice utilizando índices (X índices NEGATIVOS até o ÍNDICE INICIAL)
-# Slice utilizando índices (de um índice INICIAL até o ÚLTIMO ÍNDICE NEGATIVO)
-# Separar Lista por ' '
-# Separar Lista por ','
-# Separar Lista por '-'
-# Separar Lista especificando um número máximo de ocorrências
-# Juntar Lista juntar com ','
-# Juntar Lista juntar com '.'
-# Juntar Lista juntar com ' '
+# "Slice" utilizando índices (um ÚNICO índice especifico)
+# "Slice" utilizando índices (o ÚLTIMO índice)
+# "Slice" utilizando índices (de um índice INICIAL até um índice FINAL)
+# "Slice" utilizando índices (de um índice INICIAL até o ÚLTIMO ÍNDICE)
+# "Slice" utilizando índices (de X ÍNDICES NEGATIVOS até o ÚLTIMO ÍNDICE)
+# "Slice" utilizando índices (X índices NEGATIVOS até o ÍNDICE INICIAL)
+# "Slice" utilizando índices (de um índice INICIAL até o ÚLTIMO ÍNDICE NEGATIVO)
+# Separar uma string em uma lista utilizando ' '
+# Separar uma string em uma lista utilizando ','
+# Separar uma string em uma lista utilizando '-'
+# Separar uma string em uma lista especificando um número máximo de ocorrências
+# Juntar uma lista transformando em uma string utilizando ','
+# Juntar uma lista transformando em uma string utilizando '.'
+# Juntar uma lista transformando em uma string utilizando ' '
 # Receber Dados do Usuário (padrão)
 # Receber Dados do Usuário (convertendo de string para inteiro)
 # Operações Matemáticas (Divisão de Inteiro)
 # Operações Matemáticas (Módulos - Resto da Divisão)
 # Operações Matemáticas (Exponenciação)
 # Operações Matemáticas (Atalhos de Atribuição Rápida)
-# Operações Matemáticas (Funções de Arredondamento - padrão + para cima + para baixo)
+# Operações Matemáticas (Funções de Arredondamento => padrão + para cima + para baixo)
 # Tempo (imprimir AGORA, DIA, MÊS e ANO todos separados)
 # Tempo (passar os dados diretamente para a função de criar uma data)
 # Tempo (receber os dados de um usuário e transformar em data)
@@ -45,6 +45,7 @@
 # Números Aleatórios (gerar um valor aleatório de 0.0 a 1.0)
 # Números Aleatórios (com VALOR MÍNIMO e VALOR MÁXIMO e NÚMEROS DECIMAIS)
 # Números Aleatórios (com VALOR MÍNIMO e VALOR MÁXIMO e NÚMEROS INTEIROS)
+print('parei aqui')
 # Números Aleatórios (selecionar uma OPÇÃO ALEATÓRIA dentro de uma LISTA)
 # Números Aleatórios (selecionar de FORMA ELEATÓRIA E MÚLTIPLA  dentro de uma LISTA)
 # Números Aleatórios (EMBARALHAR os ítems de uma lista)
