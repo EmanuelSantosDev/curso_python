87bef8872b75af8fbe2e66d9651607b0e87419e1
Emanuel Santos <eu.emanuel.santos@outlook.com>
2023-01-23 23:30:00 -0300

    Update

---
+++ 06 - Tratamento de Erros e Exceções/03 - Finally.py
@@ -0,0 +1,32 @@
+# Finally
+
+
+'''
+- há casos em que mesmo que uma excessão seja tratada, uma lógica
+  precisa ser realizada após o erro ocorrer
+'''
+
+# Exemplo 1
+
+
+internet = None
+try:
+    print('Fazendo conexão com a ' + internet)
+except TypeError as erro:
+    print('Não há conexão com a Internet')
+finally:
+    print('Desfazendo a compra')
+
+
+# Exemplo 2 (tratando multiplas excessões)
+
+
+try:
+    valor = int(input('Digite um valor: '))
+    print(valor / 0)
+except ZeroDivisionError as erro:
+    print('Não é possivel dividir por zero')
+except ValueError as erro:
+    print('Favor digitar apenas números')
+finally:
+    print('A operação foi cancelada')
