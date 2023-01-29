eca2643eac539f2393637942d582dd962797c1a2
Emanuel Santos <eu.emanuel.santos@outlook.com>
2023-01-29 10:39:14 -0300

    Quando usar classes?

---
+++ 07 - Classes/12 - Quando usar Classes.py
@@ -0,0 +1,27 @@
+# ------------------------------------------------------------------------------------
+# Quando usar Classes?
+# ------------------------------------------------------------------------------------
+
+
+"""-----------------------------------------------------------------------------------
+
+1º CENÁRIO -> Escrevendo programas e rotinas simples que raramente mudam e, em geral
+possuem poucas linhas? 
+
+>>>>>>>>>>>>> NÃO USE FUNÇÕES OU CLASSES
+
+--------------------------------------------------------------------------------------
+
+2º CENÁRIO -> Tendo que reescrever a mesma funcionalidade mais de uma vez? 
+
+>>>>>>>>>>>>> USE FUNÇÕES
+
+--------------------------------------------------------------------------------------
+
+3º CENÁRIO -> Programa não é trivial e contem diversas funções, onde cada um trata 
+de um problema/tema diferente? Possuem lógicas específicas sobre determinados domínios
+de conhecimento?
+
+>>>>>>>>>>>>> USE CLASSES
+
+-----------------------------------------------------------------------------------"""
