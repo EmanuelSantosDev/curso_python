ca92566f231d5147fd16ec5a9fbcb5109ab2a12c
Emanuel Santos <eu.emanuel.santos@outlook.com>
2023-01-31 20:34:11 -0300

    Update

--- 01 - Pythonista Autodidata/03 - Funções/07 - Decorators.py
+++ 01 - Pythonista Autodidata/03 - Funções/07 - Decorators.py
@@ -5,22 +5,28 @@
 
 # altera o comportamento da função sem precisar alterar o seu código internamente
 # facilita o reuso de código
+# as funções são passadas como argumento em outra função e então chamadas dentro...
+# ...da função wrapper
 
 
-def texto(funcao):
+from datetime import datetime
+
+
+def calcular_tempo_de_duracao(funcao):
     def wrapper(a, b):
-        print('PRIMEIRO')
+        inicio = datetime.now()
         funcao(a, b)
-        print('SEGUNDO')
+        fim = datetime.now()
+        duracao = fim - inicio
+        print(f'Tempo de Duração => {duracao}')
     return wrapper
 
 
-@texto
-def soma(a, b):
-    print(a+b)
+@calcular_tempo_de_duracao
+def somar_e_calcular_tempo(a, b):
+    print(a + b)
 
 
-soma(5, 10)
-# PRIMEIRO
+somar_e_calcular_tempo(5, 10)
 # 15
-# SEGUNDO
+# Tempo de Duração => 0:00:00.000997
