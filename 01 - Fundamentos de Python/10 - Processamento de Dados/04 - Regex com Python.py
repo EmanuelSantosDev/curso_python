7ff423aa6acf9ab1f5bf459035981bbc4e76f8f0
Emanuel Santos <eu.emanuel.santos@outlook.com>
2023-02-09 23:25:02 -0300

    Update

---
+++ 01 - Fundamentos de Python/10 - Processamento de Dados/04 - Regex com Python.py
@@ -0,0 +1,9 @@
+# ------------------------------------------------------------------------------------
+# Regex ou Regular Expression
+# ------------------------------------------------------------------------------------
+
+
+# São padrões utilizados para identificar determinadas combinações caracteres 
+# abrir um arquivo "txt" para fazer os testes das expressões regulares
+# "Ctrl + f" para abrir o painel e clicar em "Use Regular Expression"
+
