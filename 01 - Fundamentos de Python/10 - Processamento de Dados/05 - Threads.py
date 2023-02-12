0c1a9be485b2555148cfbad78b589946be62ae19
Emanuel Santos <eu.emanuel.santos@outlook.com>
2023-02-12 11:06:06 -0300

    Update

---
+++ 01 - Fundamentos de Python/10 - Processamento de Dados/05 - Threads.py
@@ -0,0 +1,94 @@
+# ------------------------------------------------------------------------------------
+# Threads
+# ------------------------------------------------------------------------------------
+
+
+# uma "thread" é um fluxo separado de execução
+# isso significa que seu programa terá duas ou mais coisas acontecendo ao mesmo tempo
+# os fluxos podem ser executados de forma concorrente
+# cada Thread roda dentro de seu próprio espaço
+# se uma das Threads der erro, não impedirá a execução das demais
+
+
+import threading
+import webbrowser
+import time
+
+
+def extrar_dados_site(site):
+    print(f'Estamos navegando até o site {site}')
+    webbrowser.open_new(site)
+    for i in range(1, 21):
+        print(f'Processando dados - {i}/20')
+        time.sleep(1)
+    print('Finalizando extração de dados do site')
+
+
+def baixar_arquivos():
+    for i in range(1, 11):
+        print(f'Baixando arquivos - {i}/10')
+        time.sleep(1)
+    print('Arquivos baixados')
+
+
+def tabulacao_de_dados():
+    print('TABULAÇÃO DE DADOS: Vou ser executado somente após finalizar a nova_thread')
+
+
+# ------------------------------------------------------------------------------------
+# criando a Thread
+# ------------------------------------------------------------------------------------
+
+
+# o parâmetro "args" recebe uma "," ao final para identificá-lo como uma tupla
+# o parâmetro "daemon" define que a thread rodará em segundo plano
+# o método join() pausa a thread atual até que a thread alvo termine
+# todo o código definido após join() rodará somente após o termino da thread alvo
+# deve-se chamar join() para todas as threads, mesmo que já tenham parado
+# o fato de chamar join() libera recursos de sistema
+
+
+nova_thread = threading.Thread(target=extrar_dados_site, args=(
+    'https://www.globo.com/',), daemon=True)
+nova_thread.start()
+baixar_arquivos()
+nova_thread.join()
+tabulacao_de_dados()
+
+
+"""
+Estamos navegando até o site https://www.globo.com/
+Baixando arquivos - 1/10
+Processando dados - 1/20
+Baixando arquivos - 2/10
+Processando dados - 2/20
+Baixando arquivos - 3/10
+Processando dados - 3/20
+Baixando arquivos - 4/10
+Processando dados - 4/20
+Baixando arquivos - 5/10
+Processando dados - 5/20
+Baixando arquivos - 6/10
+Processando dados - 6/20
+Baixando arquivos - 7/10
+Processando dados - 7/20
+Baixando arquivos - 8/10
+Processando dados - 8/20
+Baixando arquivos - 9/10
+Processando dados - 9/20
+Baixando arquivos - 10/10
+Processando dados - 10/20
+Arquivos baixados
+Processando dados - 11/20
+Processando dados - 12/20
+Processando dados - 13/20
+Processando dados - 14/20
+Processando dados - 15/20
+Processando dados - 16/20
+Processando dados - 17/20
+Processando dados - 18/20
+Processando dados - 19/20
+Processando dados - 20/20
+Finalizando extração de dados do site
+TABULAÇÃO DE DADOS: Vou ser executado somente após finalizar a nova_thread
+"""
---
+++ 01 - Fundamentos de Python/10 - Processamento de Dados/06 - Multithreading.py
@@ -0,0 +1,77 @@
+# ------------------------------------------------------------------------------------
+# Multithreading
+# ------------------------------------------------------------------------------------
+
+
+# "Multithreading" é a execução em massa de várias threads em um mesmo processo
+# [thread].name retorna o nome da thread
+# neste exemplo vamos entrar em 10 sites diferentes e deixar um comentário aleatório
+# ou seja, teremos 10 threads executando paralelamente
+
+
+import threading
+import time
+import random
+
+
+def comentar(site, comentario):
+    print(f'Comentando no site {site} => {comentario}')
+    time.sleep(5)
+    print(f'Dados processados no site: {site}')
+
+
+def escolher_comentario(lista_comentarios):
+    return random.choice(lista_comentarios)
+
+
+# definindo uma lista de threads (multithreading)
+threads = []
+lista_comentarios = ['Incrivel', 'Amazing', 'Que maravilha', 'Muito bom']
+
+
+for site in range(1, 11):
+    comentario = escolher_comentario(lista_comentarios)
+    nova_thread = threading.Thread(target=comentar, args=(site, comentario))
+    threads.append(nova_thread)
+
+for thread in threads:
+    # Imprimindo o nome da Thread
+    print(f'Iniciando {thread.name}')
+    thread.start()
+
+for thread in threads:
+    thread.join()
+
+
+"""
+Iniciando Thread-1 (comentar)
+Comentando no site 1 => Muito bom
+Iniciando Thread-2 (comentar)
+Comentando no site 2 => Amazing
+Iniciando Thread-3 (comentar)
+Comentando no site 3 => Amazing
+Iniciando Thread-4 (comentar)
+Comentando no site 4 => Muito bom
+Iniciando Thread-5 (comentar)
+Comentando no site 5 => Muito bom
+Iniciando Thread-6 (comentar)
+Comentando no site 6 => Muito bom
+Iniciando Thread-7 (comentar)
+Comentando no site 7 => Que maravilha
+Iniciando Thread-8 (comentar)
+Comentando no site 8 => Amazing
+Iniciando Thread-9 (comentar)
+Comentando no site 9 => Muito bom
+Iniciando Thread-10 (comentar)
+Comentando no site 10 => Incrivel
+Dados processados no site: 1
+Dados processados no site: 4
+Dados processados no site: 3
+Dados processados no site: 2
+Dados processados no site: 7
+Dados processados no site: 6
+Dados processados no site: 5
+Dados processados no site: 9
+Dados processados no site: 10
+Dados processados no site: 8
+"""
